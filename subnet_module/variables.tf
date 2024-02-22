8ca7bccaedd5b837c42c5314fb0a0fcb904909b0
Kunduz Abdykadyrova <esengulmirlanov@esenguls-MBP.hsd1.il.comcast.net>
2024-02-21 20:25:22 -0600

    added subnet modules

---
+++ subnet_module/variables.tf
@@ -0,0 +1,10 @@
+variable "custom_vpc_id" {
+}
+variable "custom_availability_zone" {
+}
+variable "custom_cidr_block" {
+}
+variable "custom_subnet_tag" {
+}
+variable "custom_map_public_ip_on_launch" {
+}
